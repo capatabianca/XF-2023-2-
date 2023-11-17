ae5b25b8552ee19467f0e95baa5a802eabf86245
capatabianca <bianca.capata@cncn.ro>
2023-11-17 18:58:53 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF_2023
+{
+    public class Car
+    {
+        public string model;
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+
+        public Car(string nameName, string colorName, int yearValue, int maxSpeedValue)
+        {
+            name = nameName;
+            color = colorName;
+            year = yearValue;
+            maxSpeed = maxSpeedValue;
+        }
+        public Car(string nameName)
+        {
+            name = nameName;
+        }
+
+
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + "is going at the speed of" + maxSpeed);
+        }
+
+
+    }
+}
